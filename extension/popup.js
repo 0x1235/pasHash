506bc3205c11603195354854e63429eaaf131587
0x1235 <45950427+0x1235@users.noreply.github.com>
2018-12-21 17:49:34 +0300

    Add files via upload

---
+++ extension/popup.js
@@ -0,0 +1,33 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // onClick's logic below:
+	document.getElementById('viewPass').addEventListener('click', function() {
+        viewPassword();
+    });
+    document.getElementById('toHex').addEventListener('click', function() {
+        passTo(MODE.HEX);
+    });
+	document.getElementById('toBase64').addEventListener('click', function() {
+        passTo(MODE.BASE64);
+    });
+	document.getElementById('toCopy').addEventListener('click', function() {
+        ToClipboard();
+    });
+	document.getElementById('logo').addEventListener('click', function() {
+        window.open('https://github.com/0x1235/pasHash');
+    });
+	document.getElementById('password').addEventListener('keydown', function() {
+        passTo(MODE.BASE64);
+    });
+	document.getElementById('password').addEventListener('mousemove', function() {
+        passTo(MODE.BASE64);
+    });
+	
+	setInterval( function(){ passTo(MODE.CURR); }, 100 );
+	
+	isotip.init();
+});
+
+
